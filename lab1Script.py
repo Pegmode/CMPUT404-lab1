102d45c81e42e10e9ca03666c6ed3fed406cf0c3
Student <dchu@ualberta.ca>
2022-01-10 21:07:05 -0500

    updated script

--- lab1Script.py
+++ lab1Script.py
@@ -4,4 +4,7 @@ import requests
 #print(requests.__version__)
 
 #question 7
-print(requests.get("http://google.com"))
+#print(requests.get("http://google.com"))
+
+#question 8
+print(requests.get("https://raw.githubusercontent.com/Pegmode/CMPUT404-lab1/main/lab1Script.py").text)
